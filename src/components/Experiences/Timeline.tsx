8bfbde852a7d82cde255b5597b2b2c7f53e3a601
afrianluthfan <55348072+afrianluthfan@users.noreply.github.com>
2024-01-10 16:58:49 +0700

    fix: fixed tooltip not showing in the timeline component bug

--- src/components/Experiences/Timeline.tsx
+++ src/components/Experiences/Timeline.tsx
@@ -8,89 +8,89 @@ const Timeline: FC = () => (
     <div className='flex flex-col w-[70%]'>
       <div className='flex flex-row w-full gap-1 items-center'>
         <div className='w-[33%]' />
-        <div className='w-[2%] my-1'>
-          <Tooltip
-            showArrow
-            content='Sep 2021 - Oct 2021 | Became Executive Vice Chairman of the Software Engineering Quality Leadership Event.'
-          >
+        <Tooltip
+          showArrow
+          content='Sep 2021 - Oct 2021 | Became Executive Vice Chairman of the Software Engineering Quality Leadership Event.'
+        >
+          <div className='w-[2%] my-1'>
             <ProgressBar warna={2} />
-          </Tooltip>
-        </div>
+          </div>
+        </Tooltip>
       </div>
       <div className='flex flex-row w-full gap-1 items-center'>
         <div className='w-[15%]' />
-        <div className='w-[3%] my-1'>
-          <Tooltip
-            showArrow
-            content='Oct 2020 - Dec 2020 | Became an intern at Himpunan Mahasiswa S-1 Informatika Telkom University.'
-          >
+        <Tooltip
+          showArrow
+          content='Oct 2020 - Dec 2020 | Became an intern at Himpunan Mahasiswa S-1 Informatika Telkom University.'
+        >
+          <div className='w-[3%] my-1'>
             <ProgressBar warna={2} />
-          </Tooltip>
-        </div>
+          </div>
+        </Tooltip>
         <div className='w-[10%]' />
-        <div className='w-[10%] my-1'>
-          <Tooltip
-            showArrow
-            content='Jun 2021 - Dec 2021 | Became an Information, Media, and Creative staff at Formatur S-1 Rekayasa Perangkat Lunak Telkom University.'
-          >
+        <Tooltip
+          showArrow
+          content='Jun 2021 - Dec 2021 | Became an Information, Media, and Creative staff at Formatur S-1 Rekayasa Perangkat Lunak Telkom University.'
+        >
+          <div className='w-[10%] my-1'>
             <ProgressBar warna={2} />
-          </Tooltip>
-        </div>
+          </div>
+        </Tooltip>
 
-        <div className='w-[20%] my-1'>
-          <Tooltip
-            showArrow
-            content='Jan 2022 - Jan 2023 | Became Vice President at Himpunan Mahasiswa S-1 Rekayasa Perangkat Lunak Telkom University.'
-          >
+        <Tooltip
+          showArrow
+          content='Jan 2022 - Jan 2023 | Became Vice President at Himpunan Mahasiswa S-1 Rekayasa Perangkat Lunak Telkom University.'
+        >
+          <div className='w-[20%] my-1'>
             <ProgressBar warna={2} />
-          </Tooltip>
-        </div>
+          </div>
+        </Tooltip>
       </div>
       <div className='flex flex-row w-full gap-1 items-center'>
         <div className='w-[50%]' />
-        <div className='w-[9%] my-1'>
-          <Tooltip
-            showArrow
-            content='Jun 2021 - Nov 2021 | Became Executive Vice Chairman of E-Wallet User Research Challenge Event Organizer at Indosat Ooredoo'
-          >
+        <Tooltip
+          showArrow
+          content='Jun 2021 - Nov 2021 | Became Executive Vice Chairman of E-Wallet User Research Challenge Event Organizer at Indosat Ooredoo'
+        >
+          <div className='w-[9%] my-1'>
             <ProgressBar warna={1} />
-          </Tooltip>
-        </div>
-        <div className='w-[10%]'>
-          <Tooltip
-            showArrow
-            content='Jan 2022 - Jun 2022 | Worked as a Virtual Assistant at Slide Factory, Bandung.'
-          >
+          </div>
+        </Tooltip>
+        <Tooltip
+          showArrow
+          content='Jan 2022 - Jun 2022 | Worked as a Virtual Assistant at Slide Factory, Bandung.'
+        >
+          <div className='w-[10%]'>
             <ProgressBar warna={1} />
-          </Tooltip>
-        </div>
-        <div className='w-[5%] my-1'>
-          <Tooltip
-            showArrow
-            content='Jun 2022 Aug 2022 | Worked as a Database Intern at Badan Amil Zakat Nasional.'
-          >
+          </div>
+        </Tooltip>
+        <Tooltip
+          showArrow
+          content='Jun 2022 Aug 2022 | Worked as a Database Intern at Badan Amil Zakat Nasional.'
+        >
+          <div className='w-[5%] my-1'>
             <ProgressBar warna={1} />
-          </Tooltip>
-        </div>
+          </div>
+        </Tooltip>
       </div>
 
       <div className='flex flex-row w-full gap-1 items-center'>
-        <div className='w-[13%]'>
-          <Tooltip
-            showArrow
-            content='~ Aug 2019 | Graduated from SMA Negeri 2 Bogor'
-          >
+        <Tooltip
+          showArrow
+          content='~ Aug 2019 | Graduated from SMA Negeri 2 Bogor'
+        >
+          <div className='w-[13%]'>
             <ProgressBar warna={4} />
-          </Tooltip>
-        </div>
-        <div className='w-[88%] my-1'>
-          <Tooltip
-            showArrow
-            content='Aug 2019 - Nov 2023 | Studied at Telkom University majoring in Software Engineering'
-          >
+          </div>
+        </Tooltip>
+        <Tooltip
+          showArrow
+          content='Aug 2019 - Nov 2023 | Studied at Telkom University majoring in Software Engineering'
+        >
+          <div className='w-[88%] my-1'>
             <ProgressBar warna={4} />
-          </Tooltip>
-        </div>
+          </div>
+        </Tooltip>
       </div>
 
       <div className='w-full'>
